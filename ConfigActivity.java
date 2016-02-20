0669672a258df30f84f9fd1b09c8a8dc45e54b90
Andrei-Marius Longhin <axl518@cs.bham.ac.uk>
2016-02-20 21:53:52 +0000

    Added config, activity and layout

---
+++ ConfigActivity.java
@@ -0,0 +1,15 @@
+package aml.warwick_hack_part;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+
+
+    }
+}
